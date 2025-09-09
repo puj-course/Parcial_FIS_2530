5fa7d676cdc296176186c2021558068adae56ef6
thomaslopezro <151104623+thomaslopezro@users.noreply.github.com>
2025-09-09 10:44:13 -0500

    #9 feat: creacion clases usuario

---
+++ src/main/java/org/fis/grupo3/cusuario.java
@@ -0,0 +1,10 @@
+public class Usuario {
+    private String nombre;
+    private String apellido;
+    private int CC;
+    private String correo;
+    private int telefono;
+    private String tipo;
+
+    public void solicitarPrestamo(Libro libro) {}
+}

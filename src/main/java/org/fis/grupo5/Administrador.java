515ec03c6503f9f55b75435fa4bbbc844236cbbc
mate1821 <mate12185@gmail.com>
2025-11-11 10:30:07 -0500

    fix:Administrador(#101)

--- src/main/java/org/fis/grupo5/Administrador.java
+++ src/main/java/org/fis/grupo5/Administrador.java
@@ -1,10 +1,15 @@
 package org.fis;
 
-public class Administrador {
+public class Administrador implements Suscribe {
     // Este archivo es solo un ejemplo. No lo modifiques.
     // Coloca tu código en un nuevo archivo java en tu propia carpeta de grupo dentro de /fis.
     private int idAdministrador;
-    
+
+
+    public Administrador  () {
+      idAdministrador = 1 ; // se quema el adminstrador
+    }
+
     public void analizarDatos() {                //analiza los datos del sistemas
     }
     public void cantidadLibrosDisponibles() {            //devuelve un entero de la cantidad de libros disponibles para prestar
@@ -14,6 +19,11 @@ public class Administrador {
     public int cantidadLibrosDevueltosUltimoMes() {        //devuelve un entero de la cantidad de libros devueltos el ultimo mes
     }
     public list<String> listarUsuariosConRetrasios() {    //devuelve una lista de los usuarios que estan enretraso
+    }
+    @Override
+    public void actualizar(String mensaje) {
+        // esto toco cambiarlo
+        System.out.println(mensaje);
     }
 
     public static void main(String[] args) {
